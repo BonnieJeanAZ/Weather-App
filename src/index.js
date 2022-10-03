ef89111b542c66bf16ee9ec9f6e089c1108e4124
Bonnie Runyan <bonniejeanaz@gmail.com>
2022-10-03 15:42:43 -0700

    Updated Layout,  Icons, and default search.

--- src/index.js
+++ src/index.js
@@ -39,19 +39,35 @@ function showTemperature(response) {
   let temperature = Math.round(response.data.main.temp);
   let city = response.data.name;
   let description = response.data.weather[0].description;
+  let wind = Math.round(response.data.wind.speed) + "mph wind";
   console.log(response.data.name);
   console.log(response);
 
-  let temperatureElement = document.querySelector("#temp");
+  let temperatureElement = document.querySelector("h2 #temp");
   temperatureElement.innerHTML = `${temperature}°`;
 
   let cityElement = document.querySelector("h1");
   cityElement.innerHTML = `${city}`;
 
-  let descriptionElement = document.querySelector("#describe");
+  let descriptionElement = document.querySelector("h4 #describe");
   descriptionElement.innerHTML = `${description}`;
 
+  let windElement = document.querySelector("h4 #wind");
+  windElement.innerHTML = `${wind}`;
+
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+
   form.reset();
+}
+
+function search(city) {
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
 }
 
 //bonus week 5
@@ -73,15 +89,17 @@ function currentPosition(position) {
 //celcius
 let celcius = document.querySelector("#celcius-link");
 let fahrenheit = document.querySelector("#fahrenheit-link");
-let changeTemp = document.querySelector(".changeTemp");
+let changeTemp = document.querySelector("changeTemp");
 
 function changeCelcius(event) {
   event.preventDefault();
-  changeTemp.innerHTML = "37";
+  temp.innerHTML = "37";
 }
 function changeFahrenheit(event) {
   event.preventDefault();
-  changeTemp.innerHTML = "99";
+  temp.innerHTML = "99";
 }
 celcius.addEventListener("click", changeCelcius);
 fahrenheit.addEventListener("click", changeFahrenheit);
+
+search("New York");
